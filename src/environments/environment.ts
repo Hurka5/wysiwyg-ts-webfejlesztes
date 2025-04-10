03280c4bacd7be6ef72d2668a667cead0b307a7b
Hurka5 <hurkagergo5@gmail.com>
2025-04-10 15:00:33 +0200

    Backup 04.10

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "wysiwyg-ts-webfejlesztes.firebaseapp.com",
+    projectId: "wysiwyg-ts-webfejlesztes",
+    storageBucket: "wysiwyg-ts-webfejlesztes.appspot.com",
+    messagingSenderId: "XXXX",
+    appId: "XXXX",
+    databaseURL: "https://your-project-id.firebaseio.com" // optional if using RTDB
+  }
+};
+
