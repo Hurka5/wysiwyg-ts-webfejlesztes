4609d2a5da4fad45f7038934021d0a71d587acca
Hurka5 <hurkagergo5@gmail.com>
2025-04-11 23:45:34 +0200

    firebase fix

--- src/app/scripts-list/scripts-list.component.ts
+++ src/app/scripts-list/scripts-list.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-scripts-list',
-  imports: [],
-  templateUrl: './scripts-list.component.html',
-  styleUrl: './scripts-list.component.css'
+  selector: "app-scripts-list",
+  imports: [CommonModule],
+  templateUrl: "./scripts-list.component.html",
+  styleUrl: "./scripts-list.component.css",
 })
 export class ScriptsListComponent {
-
+  items = ["a", "b", "c"];
+  addItem() {
+    this.items.push("NEW ITEM");
+  }
+  test() {
+    alert("MEOW");
+  }
 }
